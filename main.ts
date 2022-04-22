6bb47c4b55e9f60b7c4f63fdb3226233e26fe702
Leon <89528170+einfachlxon@users.noreply.github.com>
2022-04-22 11:42:26 +0200

    Bluetooth Connection mit eingebaut und für Smartphone Steuerung vorbereitet

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
+bluetooth.onBluetoothConnected(function () {
+    sprite = 2
+    if (next == 2) {
+        basic.showString("READY")
+        basic.setLedColor(0x00ff00)
+    }
+})
+bluetooth.onBluetoothDisconnected(function () {
+    if (next == 1) {
+        basic.showString("CONNECT YOUR PHONE")
+        basic.setLedColor(0xffff00)
+        basic.pause(100)
+        basic.setLedColor(0xffff00)
+    }
+})
+let sprite = 0
 let next = 0
+next = 0
 for (let index = 0; index < 4; index++) {
     basic.setLedColor(0xff0000)
     basic.pause(200)
@@ -8,11 +25,7 @@ for (let index = 0; index < 4; index++) {
 basic.showString("SDM", 250)
 next = 1
 basic.forever(function () {
-    if (next == 1) {
-        basic.showString("READY")
-        basic.setLedColor(0x00ff00)
-    }
-    next = 2
+    basic.showNumber(devices.signalStrength())
 })
 basic.forever(function () {
     if (next == 2) {
